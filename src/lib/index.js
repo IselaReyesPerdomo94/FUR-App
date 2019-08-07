344502bffc3a52391bd65e5ddf970ae6aaf2cb91
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-08-06 23:44:11 -0500

    Agregando firebase al proyecto, jalando función de registrar y validando, correo electronico, contraseña y checkbox

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,38 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+const register = (emailInput, passwordInput, passwordConfirmInput, acceptRegisterInput) => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const confirmPass = passwordConfirmInput.value;
+    const accept = acceptRegisterInput.checked;
+    if (email.length < 4) {
+        alert('Tu email no es válido');
+        return;
+    }
+    if (password.length < 6) {
+        alert('Tu contraseña debe contener al menos 6 caracteres');
+        return;
+    }
+    if (password !== confirmPass) {
+        alert('La contraseña no concuerda');
+        return;
+    }
+    if (!accept) {
+        alert('Por favor acepta los términos y condiciones para registrarte');
+        return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(function(error) {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ...
+            console.log(email);
+            console.log(password);
+
+        });
+    email - password.html
+}
+
+export { register };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
+import { register } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+//variables
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const passwordConfirmInput = document.getElementById('confirm-p');
+const acceptRegisterInput = document.getElementById('accept');
+const readyRegister = document.getElementById('ready');
 
-myFunction();
+
+console.log('hello')
+readyRegister.addEventListener('click', () => register(emailInput, passwordInput, passwordConfirmInput, acceptRegisterInput));
