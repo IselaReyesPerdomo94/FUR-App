1fda7c9c6eeee24369aa42ea31680a1a6dc1c503
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-08-07 14:06:04 -0500

    Se agregó condición para evaluar nombre de Usuario

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 import { register } from './lib/index.js';
 
-//variables
+//Const
+const userNameInput = document.getElementById('user-name');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const passwordConfirmInput = document.getElementById('confirm-p');
 const acceptRegisterInput = document.getElementById('accept');
 const readyRegister = document.getElementById('ready');
 
-
-console.log('hello')
-readyRegister.addEventListener('click', () => register(emailInput, passwordInput, passwordConfirmInput, acceptRegisterInput));
+//Events
+readyRegister.addEventListener('click', () => register(userNameInput, emailInput, passwordInput, passwordConfirmInput, acceptRegisterInput));
