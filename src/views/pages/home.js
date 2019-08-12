91918d3b5d603a450abffb66af5b2ab450e9a53d
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-08-11 19:18:55 -0500

    se agrego icono de memes y veterinario con flexbox

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -3,7 +3,27 @@ let home = {
     render: async() => {
         let view = /*html*/ `
             <section class="home">
-                <h1> This is our home </h1>
+            <hr>
+            <div class="flex-container">
+            <div>
+            <img src="img/happy-emoticon-with-one-tooth.svg" alt="Memes" class="memes">
+            <p>Memes</p>
+            </div>
+            <div>
+            <img src="img/vaccine.svg" alt="Veterinario" class="veterinario">
+            <p>Veterinario</p>
+            </div>
+            <div>
+            <img src="" alt="Veterinario" class="veterinario">
+            </div>
+            <div>
+            <img src="img/vaccine.svg" alt="Veterinario" class="veterinario">
+            </div>
+            </div>
+            
+            
+
+            <hr>
             </section>
         `
         return view
