973e312a839d9867e38a500502b3e7ee88f63b58
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-08-18 08:25:11 -0500

    se identaron div en home

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -43,17 +43,17 @@ const home = {
             <div id="root">
                     </div>
             </div>
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
- <div class="modal-dialog" role="document">
-   <div class="modal-content">
-     <div class="modal-header">
-       <h5 class="modal-title" id="exampleModalLabel">¡Realiza una publicación!
-       <img src="img/kitty.svg" alt="gatito" class="kitty">
-       </h5>
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-         <span aria-hidden="true">&times;</span>
-       </button>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+          <div class="modal-content">
+          <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">¡Realiza una publicación!
+          <img src="img/kitty.svg" alt="gatito" class="kitty">
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
            </div>
            <div class="modal-body">
            <textarea placeholder="¿En que piensas?" class="publicacion" id="publicacion"></textarea>
